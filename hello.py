cd30f3baca14aa1aa0e3e71096e4bb1d65eff5a1
davidmorseiii <dmmorse3@gmail.com>
2025-05-27 22:43:22 -0600

    Added attempt tracking functionality to program and converted final print statement to printf to tidy things

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 #Basic Python Program
 
 user_input = ""
+attempts = 0
 
 while user_input != "green":
     user_input = input("What is the magic color? ")
+    attempts += 1
     if user_input != "green":
         print("That is not the magic color")
 
-print("Correct! " + user_input + " is the magic color!")
+print(f"Correct! {user_input} is the magic color! It took you {attempts} tries.")
